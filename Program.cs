e609a71d74af626636479940bffc23e8662218b2
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-03-17 20:49:39 -0300

    top 10 escrevimentos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.CursorVisible = false;
+
+Console.WriteLine("Pressione uma tecla para continuar... ooooomaga");
+Console.ReadKey();
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("whataheeelll");
+Console.WriteLine("");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("ou mai ga");
+Console.WriteLine("");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("no waay yeyeye");
+Console.WriteLine("");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("uou");
+Console.ResetColor();
+Console.ReadKey();
+
+Console.CursorVisible = true;
